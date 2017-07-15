8682873cb82537d6705c05d0f4bbaf95a1d49f51
Boris Emorine <boris.emorine@gmail.com>
2017-07-15 10:11:00 -0700

    Add ARBarCharts framework

---
+++ ARBarCharts/ARBarCharts.h
@@ -0,0 +1,19 @@
+//
+//  ARBarCharts.h
+//  ARBarCharts
+//
+//  Created by Bobo on 7/15/17.
+//  Copyright © 2017 Boris Emorine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ARBarCharts.
+FOUNDATION_EXPORT double ARBarChartsVersionNumber;
+
+//! Project version string for ARBarCharts.
+FOUNDATION_EXPORT const unsigned char ARBarChartsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARBarCharts/PublicHeader.h>
+
+
