0b5a09ca7d8a6a65b1b571794d3ab355e2394772
Boris Emorine <boris.emorine@gmail.com>
2017-07-16 11:15:39 -0700

    Add new ARDataSeries object

---
+++ ARBarCharts/ARDataSeries.swift
@@ -0,0 +1,25 @@
+//
+//  ARDataSeries.swift
+//  ARBarCharts
+//
+//  Created by Bobo on 7/16/17.
+//  Copyright © 2017 Boris Emorine. All rights reserved.
+//
+
+import Foundation
+
+class ARDataSeries: ARBarChartsDataSource, ARBarChartsDelegate {
+    func numberOfSeries(in barChart: ARBarChartNode) -> Int {
+        
+    }
+    
+    func barChart(_ barChart: ARBarChartNode, numberOfValuesInSeries series: Int) -> Int {
+        
+    }
+    
+    func barChart(_ barChart: ARBarChartNode, valueAtIndex index: Int, forSeries series: Int) -> Double {
+        
+    }
+    
+    
+}
