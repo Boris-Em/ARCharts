8d20024138de63dc7d2d3c06250ca5dda25a9182
Boris Emorine <boris.emorine@gmail.com>
2017-07-16 19:07:06 -0700

    Add first convenience functions for ARBarChartNode

--- ARBarCharts/ARBarChart.swift
+++ ARBarCharts/ARBarChart.swift
@@ -14,4 +14,48 @@ public class ARBarChartNode: SCNNode {
     weak var dataSource: ARBarChartsDataSource?
     weak var delegate: ARBarChartsDelegate?
     
+    // MARK: - Convenience Functions
+    
+    private lazy var numberOfSeries: Int = {
+        guard let dataSource = self.dataSource else {
+            return 0
+        }
+        
+        return dataSource.numberOfSeries(in: self)
+    }()
+    
+    private func numberOfValues(forSeries series: Int) -> Int {
+        guard let dataSource = dataSource else {
+            return 0
+        }
+        
+        return dataSource.barChart(self, numberOfValuesInSeries: series)
+    }
+    
+    private func valueAtIndex(index: Int, forSeries series: Int) -> Double? {
+        guard let dataSource = dataSource else {
+            return nil
+        }
+        
+        return dataSource.barChart(self, valueAtIndex: index, forSeries: series)
+    }
+    
+    private lazy var maxValue: Double? = {
+        var seriesIndex = 0
+        var maxValue = Double.leastNonzeroMagnitude
+        while seriesIndex < self.numberOfSeries {
+            var valuesIndex = 0
+            while valuesIndex < self.numberOfValues(forSeries: seriesIndex) {
+                guard let value = self.valueAtIndex(index: valuesIndex, forSeries: seriesIndex) else {
+                    return nil
+                }
+                maxValue = max(maxValue, value)
+                valuesIndex += 1
+            }
+            seriesIndex += 1
+        }
+        
+        return maxValue
+    }()
+    
 }
